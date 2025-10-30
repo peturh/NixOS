9dc447d36d900ce35a35a73319a77cad7d50736e
petur <petur.hjartarson@gmail.com>
2025-10-30 07:40:25 +0100

    fix: use ms-edge as default

--- modules/programs/browser/microsoft-edge/default.nix
+++ modules/programs/browser/microsoft-edge/default.nix
@@ -3,7 +3,23 @@
   inputs,
   ...
 }: {
-  environment.systemPackages = with pkgs; [
-    microsoft-edge
+  home-manager.sharedModules = [
+    (_: {
+      home.packages = with pkgs; [
+        microsoft-edge
+      ];
+
+      # Set Microsoft Edge as the default browser for all web links
+      xdg.mimeApps = {
+        enable = true;
+        defaultApplications = {
+          "text/html" = "microsoft-edge.desktop";
+          "x-scheme-handler/http" = "microsoft-edge.desktop";
+          "x-scheme-handler/https" = "microsoft-edge.desktop";
+          "x-scheme-handler/about" = "microsoft-edge.desktop";
+          "x-scheme-handler/unknown" = "microsoft-edge.desktop";
+        };
+      };
+    })
   ];
 }
