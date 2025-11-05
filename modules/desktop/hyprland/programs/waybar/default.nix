f8774f43fced9d4570f9155c138cc976af534f71
petur <petur.hjartarson@gmail.com>
2025-11-05 10:12:04 +0100

    fix: separator

--- modules/desktop/hyprland/programs/waybar/default.nix
+++ modules/desktop/hyprland/programs/waybar/default.nix
@@ -61,7 +61,7 @@
                 "*" = [1 2 3 4 5 6 7 8 9 10];
               };
               format = "{id} {windows}";
-              format-window-separator = " |";
+              format-window-separator = " | ";
               window-rewrite-default = "";
               window-rewrite = {
                 "firefox" = "󰈹";
